1c08aaf6547ba829344c74fb6645950e7308ccb6
user <katya.rotar02@gmail.com>
2023-10-01 20:14:27 +0300

    Add project files.

---
+++ Task_1/Child.cs
@@ -0,0 +1,16 @@
+﻿class Child : Person
+{
+    public Child(string name, int age) : base(name, age)
+    {  
+    }
+    public override int Age {
+        get { return base.Age; }
+        set {
+            if (value > 15)
+            {
+                throw new ArgumentException("Child's age must be less than 15!");
+            }
+            base.Age = value;
+        }
+    }
+}
