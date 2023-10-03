b18e0e6c6f5dc70421d65b479dc750998c680fb1
user <katya.rotar02@gmail.com>
2023-10-03 22:49:38 +0300

    Task_4

---
+++ Task_4/Exception.cs
@@ -0,0 +1,20 @@
+﻿class InvalidSongException : Exception{
+    public InvalidSongException(string message = "Invalid song.") : base(message)
+    {
+    }
+}
+class InvalidArtistNameException : InvalidSongException {
+    public InvalidArtistNameException(string message = "Artist name should be between 3 and 20 symbols.") : base(message) { }
+}
+class InvalidSongNameException : InvalidSongException{
+    public InvalidSongNameException(string message = "Song name should be between 3 and 30 symbols.") : base(message) { }
+}
+class InvalidSongLengthException : InvalidSongException {
+    public InvalidSongLengthException(string message = "Invalid song length.") : base(message) { }
+}
+class InvalidSongMinutesException : InvalidSongLengthException {
+    public InvalidSongMinutesException(string message = "Song minutes should be between 0 and 14.") : base(message) { }
+}
+class InvalidSongSecondsException : InvalidSongLengthException {
+    public InvalidSongSecondsException (string message = "Song seconds should be between 0 and 59.") : base(message) { }
+}
---
+++ Task_4/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    private static void Main(string[] args){
+        int numberSong = int.Parse(Console.ReadLine());
+        List<Song> song = new List<Song>();
+        int SongAdded = 0;
+
+        for (int i = 0; i < numberSong; i++)
+        {
+            try
+            {
+                string[] songInfo = Console.ReadLine().Split(';');
+                string artistName = songInfo[0];
+                string songName = songInfo[1];
+                string[] songLength = songInfo[2].Split(':');
+                if (songLength.Length != 2)
+                {
+                    throw new InvalidSongLengthException();
+                }
+                int minutes = int.Parse(songLength[0]);
+                int seconds = int.Parse(songLength[1]);
+                Song songs = new Song(artistName, songName, minutes, seconds);
+                song.Add(songs);
+                Console.WriteLine("Song added.");
+                SongAdded++;
+
+            }
+            catch (InvalidSongException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        int totalHour = 0;
+        int totalMinutes = 0;
+        int totalSeconds = 0;
+
+        for(int i = 0; i < song.Count; i++)
+        {
+            totalMinutes += song[i].Minutes;
+            totalSeconds += song[i].Seconds;
+        }
+        totalMinutes += totalSeconds / 60;
+        totalHour = totalMinutes / 60;
+        totalSeconds %= 60; //ділимо націло на 60 це знаходимо скільки секунд залишилося 
+        Console.WriteLine($"\nSongs added: {SongAdded}" +
+            $"\nPlaylist length: {totalHour}h {totalMinutes}m {totalSeconds}s");
+    }    
+}
